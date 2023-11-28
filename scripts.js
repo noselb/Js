1ea4b1985fbd8aec0b09e612d4b78ce357f0016a
noselb <amfibii@mail.ru>
2023-11-28 18:23:43 +0300

    add:hw8

--- scripts.js
+++ scripts.js
@@ -1,217 +1,207 @@
 "use strict";
 
-// //Task 1
-// {
-//   for (let i = 1; i <= 50; i++) {
-//     console.log(i);
-//   }
-// }
+let html = document.querySelector("html");
+html.setAttribute("lang", "eu");
+console.log(html);
 
-// {
-//   for (let j = 38; j >= 8; j--) {
-//     console.log(j);
-//   }
-// }
-// //Task 2
+let metaUtf8 = document.createElement("meta");
+metaUtf8.setAttribute("charset", "UTF-8");
+document.head.appendChild(metaUtf8);
 
-// {
-//   let i = 89;
-//   while (i >= 11) {
-//     document.write(i + "</br>");
-//     i--;
-//   }
-// }
-// //Task 3
-// {
-//   let sum = 0;
-//   for (let i = 0; i <= 100; i++) {
-//     console.log(sum + i);
-//     sum = sum + i;
-//   }
-// }
-// //Task 4
-// {
-//   let a = 5;
-//   let sum = 0;
-//   for (let i = 1; i <= a; i++) {
-//     sum = 0;
-//     for (let j = 1; j <= i; j++) {
-//       sum += j;
-//     }
-//     console.log("sum" + i + " = " + sum);
-//   }
-// }
-// //Task 5
-// {
-//   for (let i = 8; i <= 86; i++) {
-//     if (i % 2 == 0) continue;
-//     console.log(i);
-//   }
-// }
+let titlePage = document.createElement("title");
+titlePage.innerHTML = "Home Work Eight";
+document.head.appendChild(titlePage);
 
-// //Task 6
-// {
-//   for (let i = 2; i <= 10; i++) {
-//     for (let j = 2; j <= 10; j++) {
-//       console.log(`${i}*${j} = ${i * j}`);
-//     }
-//   }
-// }
-// //Task 7
-// {
-//   let n = 1000;
-//   let num = 0;
-//   while (n >= 50) {
-//     n = n / 2;
-//     num++;
-//     console.log(num);
-//   }
-// }
-// //Task 8
-// {
-//   let str = "";
-//   let num = 0;
-//   let sum = 0;
-//   let iteration = 0;
-//   while (true) {
-//     let user = prompt("Введите число с клавиатуры");
-//     sum = parseInt(user) + sum;
-//     iteration++;
-//     if (user == str || user == num) {
-//       break;
-//     }
-//   }
-//   console.log(`Сумма чисел равна ${sum}`);
-//   console.log(`Среднее арифметическое равно ${sum / iteration}`);
-// }
-// //Task 9
+//body
 
-//Array
+let container = document.createElement("div");
+container.className = "container";
+document.body.prepend(container);
 
-//Task 1
-// {
-//   let arr = [1, 2, 3, 4, 5];
-//   for (let i = 0; i < arr.length; i++) alert(arr[i]);
-// }
-// {
-//   let numbers = [1, 2, 3, 4, 5];
-//   for (let num of numbers) {
-//     alert(num);
-//   }
-// }
+const header = document.createElement("header");
+header.classList.add("header");
+container.append(header);
 
-//Task 2
+const titleContainer = document.createElement("div");
+titleContainer.classList.add("title-container");
+header.append(titleContainer);
 
-// {
-//   let arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
-//   alert(arr[13]);
-//   alert(arr[7]);
-//   alert(arr[5]);
-// }
+const h1 = document.createElement("h1");
+h1.className = "title-container__title";
+h1.innerHTML = "Choose Your Option";
+titleContainer.appendChild(h1);
 
-//Task 3
+const span = document.createElement("span");
+span.classList.add("title-container__text");
+span.innerHTML =
+  "But I must explain to you how all this mistaken idea of denouncing";
+titleContainer.appendChild(span);
 
-// {
-//   let arr1 = [];
-//   let arr2 = [];
-//   let result = 0;
-//   let i = 23;
-//   let j = 0;
+const main = document.createElement("main");
+main.classList.add("main");
+container.appendChild(main);
 
-//   for (; i <= 57; i++) {
-//     arr1.push(i);
-//     result += i;
-//   }
-//   console.log(arr1);
-//   console.log(result);
+const divFreelancer = document.createElement("div");
+divFreelancer.classList.add("block");
+main.appendChild(divFreelancer);
 
-// }
+let subtitleFreelancer = document.createElement("h3");
+subtitleFreelancer.classList.add("subtitle");
+subtitleFreelancer.innerHTML = "FREELANCER";
+divFreelancer.appendChild(subtitleFreelancer);
 
-//Task 4
+let titleFreelancer = document.createElement("h2");
+titleFreelancer.classList.add("title");
+titleFreelancer.innerHTML = "Initially designed to ";
+divFreelancer.appendChild(titleFreelancer);
 
-// {
-//   let arrStr = ["10", "20", "30", "50", "235", "3000"];
-//   for (let i = 0; i <= arrStr.length; i++) {
-//     let num = String(arrStr[i]);
-//     let char = num[0];
-//     if (char == 1 || char == 2 || char == 5) {
-//       console.log(num);
-//     }
-//   }
-// }
+let spanFreelancer = document.createElement("span");
+spanFreelancer.classList.add("span");
+spanFreelancer.innerHTML =
+  "But I must explain to you how all this mistaken idea of denouncing ";
+divFreelancer.appendChild(spanFreelancer);
 
-//Task 5
-// {
-//   let arrWeeks = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ];
-//   for (let i = 0; i < arrWeeks.length; i++) {
-//     let num = arrWeeks[i];
-//     console.log(num);
-//   }
-// }
+let buttonFreelancer = document.createElement("button");
+buttonFreelancer.classList.add("button");
+buttonFreelancer.innerHTML = "START HERE";
+divFreelancer.appendChild(buttonFreelancer);
 
-//Task 6
-// {
-//   let arr = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ];
-//   arr.push("Another day");
-//   console.log(arr);
-//   console.log(arr[arr.length - 1]);
-// }
+let cloneDivFreelancer = divFreelancer.cloneNode(true);
+main.append(cloneDivFreelancer);
 
-//Task 7
-// {
-//   let sum = [];
-//   let text = +prompt("Введите значения");
-//   while (text != "") {
-//     text = +prompt("Введите значения");
-//     sum.push(text);
-//   }
-//   sum.sort(function (a, b) {
-//     return a - b;
-//   });
-//   alert(sum);
-// }
-//Task 8
-// {
-//   let arr = [12, false, "Текст", 4, 2, -5, 0];
-//   arr.reverse();
-//   console.log(arr);
-// }
-//Task 9
+cloneDivFreelancer.classList.remove("block");
+cloneDivFreelancer.classList.add("studio-block");
+cloneDivFreelancer.querySelector("h3").innerHTML = "STUDIO";
 
-// {
-//   let arr = [5, 9, 21, , , 9, 78, , , , 6];
-//   let res = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (i in arr) {
-//       res.push(arr[i]);
-//     }
-//   }
-//   console.log(arr);
-//   console.log(res);
-// }
-//task 10
+let style = document.createElement("style");
+style.innerHTML = `
 
-{
-  let arr = [48, 9, 0, 4, 21, 2, 1, 0, 8, 84, 76, 8, 4, 13, 2];
-  const arr1 = arr.slice(3, 7);
-  let result = arr1.reduce((sum, elem) => sum + elem, 0);
+    .header{
+        display: flex;
+        justify-content: center;
+    }
 
-  console.log(arr1);
-  console.log(result);
-}
+    .main{
+        display: flex;
+        justify-content: center;
+    }
+
+    .title-container{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        width:464px;
+        height:104px
+    }
+
+    .title-container__title{
+        width:354px;
+        height:48px
+        font-size: 36px;
+        font-weight: 400;
+        text-align: center;
+    }
+
+    .title-container__text{
+        color: #9FA3A7;
+        text-align: center;
+        font-size: 14px;
+        font-weight: 400;
+    }
+
+    .block{
+        width:401px;
+        height:480px;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        background:#E8E9ED;
+    }
+    
+    .subtitle{
+        width:107px;
+        height:15px;
+        padding-top: 81px;
+        color: #9FA3A7;
+        font-size: 12px;
+        font-weight: 700;
+        text-align: center;
+    }
+
+    .title{
+        width:210px;
+        height:92px;
+        text-align: center;
+        font-size: 36px;
+        font-weight: 400;
+    }
+
+    .span{
+        width:210px;
+        height:44px;
+        padding-bottom: 56px;
+        color: #9FA3A7;
+        font-size: 12px;
+        font-weight: 400;
+        text-align: center;
+
+    }
+    
+
+
+    .button{
+        width:176px;
+        height:73px;
+        border-radius:45%;
+        border:3px solid yellow;
+        background:white;
+        text-align: center;
+        font-size: 12px;
+        font-weight: 700;
+        
+        
+    }
+    .button:hover{
+        color:orange;
+        cursor:pointer;
+
+    }
+
+    .button{
+        opacity: 0;
+        animation: ani 8s forwards;
+      }
+      @keyframes ani {
+        0% {opacity: 0;}
+        100% {opacity: 1;}
+ 
+         }
+
+         .studio-block{
+            width:401px;
+            height:480px;
+            display:flex;
+            flex-direction: column;
+            align-items: center;
+            background: #8F75BE;
+        }
+
+        .studio-block .subtitle{
+            color:#FFC80A;
+        }
+
+        .studio-block .title{
+            color:white;
+        }
+
+        .studio-block .span{
+            color:white;
+        }
+
+        .studio-block .button{
+            color:white;
+            background: #8F75BE;
+        }
+
+`;
+document.head.appendChild(style);
