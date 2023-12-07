8cfe3ec3f2d641a778751cc95fde2c7506d8a46b
noselb <amfibii@mail.ru>
2023-12-07 17:49:05 +0300

    add:hw10

--- scripts.js
+++ scripts.js
@@ -1,48 +1,45 @@
 "use strict";
+//Реализация 1
+{
+  let GirlsContacts = function () {
+    this.cont = [];
+    this.addNewContact = function () {
+      this.name = prompt("Введите Имя");
+      this.age = +prompt("Введите возраст");
+      this.phoneNumber = prompt("Введите номер телефона");
+      this.email = prompt("Введите свой Email");
+      this.chekInfo();
+      this.cont.push({
+        name: this.name,
+        age: this.age,
+        phoneNumber: this.phoneNumber,
+        email: this.email,
+      });
+    };
+    this.chekInfo = function () {
+      if (this.age <= 18) {
+        return alert("Ваш возраст не соответствует требованиям");
+      }
+    };
+  };
 
-function createList() {
-  const input = document.querySelector(".input-text");
-  const button = document.querySelector(".submit");
+  let girls = new GirlsContacts([]);
+  girls.addNewContact();
+  console.log(girls);
 
-  input.addEventListener("change", createNote);
+  let Boys = function (name, age, phoneNumber, email) {
+    this.__proto__ = girls;
+    this.name = name;
+    this.age = age;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  };
 
-  function createNote(event) {
-    const value = event.target.value;
-    console.log(value);
-    input.value = "";
-
-    const tasks = document.querySelector(".list");
-
-    const listNotes = document.createElement("div");
-    listNotes.className = "list-notes";
-    tasks.append(listNotes);
-
-    const task = document.createElement("span");
-    task.className = "notes";
-    task.innerHTML = value;
-    listNotes.prepend(task);
-
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.className = "check";
-    listNotes.prepend(checkbox);
-
-    checkbox.addEventListener("click", checked);
-    function checked() {
-      task.classList.toggle("active");
-    }
-
-    task.addEventListener("click", changeText);
-    function changeText(event) {
-      event.target.innerHTML = prompt("Change");
-    }
-
-    button.addEventListener("click", reset);
-
-    function reset() {
-      const deleteElem = document.querySelector(".list");
-      deleteElem.innerHTML = " ";
-    }
-  }
+  let boys = new Boys(
+    prompt("Введите Имя"),
+    +prompt("Введите возраст"),
+    prompt("Введите номер телефона"),
+    prompt("Введите свой Email")
+  );
+  console.log(boys);
 }
-createList();
