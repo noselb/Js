77958161706bc527658b341b507a4ca21d84b157
noselb <amfibii@mail.ru>
2023-12-17 23:20:25 +0300

    add:hw10.1

--- scripts.js
+++ scripts.js
@@ -1,217 +1,97 @@
 "use strict";
 
-// //Task 1
-// {
-//   for (let i = 1; i <= 50; i++) {
-//     console.log(i);
-//   }
-// }
+let Device = function () {
+  let status = false;
 
-// {
-//   for (let j = 38; j >= 8; j--) {
-//     console.log(j);
-//   }
-// }
-// //Task 2
+  this.deviceOn = function () {
+    status = true;
+    console.log(`The ${this.name} ${this.model} is on`);
+  };
 
-// {
-//   let i = 89;
-//   while (i >= 11) {
-//     document.write(i + "</br>");
-//     i--;
-//   }
-// }
-// //Task 3
-// {
-//   let sum = 0;
-//   for (let i = 0; i <= 100; i++) {
-//     console.log(sum + i);
-//     sum = sum + i;
-//   }
-// }
-// //Task 4
-// {
-//   let a = 5;
-//   let sum = 0;
-//   for (let i = 1; i <= a; i++) {
-//     sum = 0;
-//     for (let j = 1; j <= i; j++) {
-//       sum += j;
-//     }
-//     console.log("sum" + i + " = " + sum);
-//   }
-// }
-// //Task 5
-// {
-//   for (let i = 8; i <= 86; i++) {
-//     if (i % 2 == 0) continue;
-//     console.log(i);
-//   }
-// }
+  this.deviceOff = function () {
+    status = false;
+    console.log(`The ${this.name} ${this.model} is off`);
+  };
 
-// //Task 6
-// {
-//   for (let i = 2; i <= 10; i++) {
-//     for (let j = 2; j <= 10; j++) {
-//       console.log(`${i}*${j} = ${i * j}`);
-//     }
-//   }
-// }
-// //Task 7
-// {
-//   let n = 1000;
-//   let num = 0;
-//   while (n >= 50) {
-//     n = n / 2;
-//     num++;
-//     console.log(num);
-//   }
-// }
-// //Task 8
-// {
-//   let str = "";
-//   let num = 0;
-//   let sum = 0;
-//   let iteration = 0;
-//   while (true) {
-//     let user = prompt("Введите число с клавиатуры");
-//     sum = parseInt(user) + sum;
-//     iteration++;
-//     if (user == str || user == num) {
-//       break;
-//     }
-//   }
-//   console.log(`Сумма чисел равна ${sum}`);
-//   console.log(`Среднее арифметическое равно ${sum / iteration}`);
-// }
-// //Task 9
+  this.chek = function () {
+    if (status) {
+      console.log("You can do something");
+    } else {
+      console.log("On should turn on your device");
+    }
+  };
+};
 
-//Array
+let CalcSdc = function (name, model, setOfNumbers) {
+  Device.apply(this);
 
-//Task 1
-// {
-//   let arr = [1, 2, 3, 4, 5];
-//   for (let i = 0; i < arr.length; i++) alert(arr[i]);
-// }
-// {
-//   let numbers = [1, 2, 3, 4, 5];
-//   for (let num of numbers) {
-//     alert(num);
-//   }
-// }
+  (this.name = name), (this.model = model), (this.setOfNumbers = setOfNumbers);
 
-//Task 2
+  this.sum = function () {
+    let arrNum = [];
+    let numbers;
 
-// {
-//   let arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
-//   alert(arr[13]);
-//   alert(arr[7]);
-//   alert(arr[5]);
-// }
+    while (numbers !== 0 || "") {
+      numbers = +prompt("Введите значение");
+      arrNum.push(numbers);
+    }
+    console.log(arrNum);
+    let sumOfNum = arrNum.reduce(function (sum, elem) {
+      return sum + elem;
+    });
+  };
 
-//Task 3
+  this.minus = function () {
+    let arrNum = [];
+    let numbers;
 
-// {
-//   let arr1 = [];
-//   let arr2 = [];
-//   let result = 0;
-//   let i = 23;
-//   let j = 0;
+    while (numbers !== 0 || "") {
+      numbers = +prompt("Введите значение");
+      arrNum.push(numbers);
+    }
+    let minOfNum = arrNum.reduce(function (sum, elem) {
+      return sum - elem;
+    });
+    console.log(minOfNum);
+  };
 
-//   for (; i <= 57; i++) {
-//     arr1.push(i);
-//     result += i;
-//   }
-//   console.log(arr1);
-//   console.log(result);
+  this.multiplication = function () {
+    let arrNum = [];
+    let numbers;
 
-// }
+    while (numbers !== 0 || "") {
+      numbers = +prompt("Введите значение");
+      arrNum.push(numbers);
+    }
+    arrNum.pop();
+    let multiOfNum = arrNum.reduce(function (sum, elem) {
+      return sum * elem;
+    });
+  };
+};
 
-//Task 4
+let calcSdc = new CalcSdc(
+  "Calculator",
+  "SDC-888T",
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+);
 
-// {
-//   let arrStr = ["10", "20", "30", "50", "235", "3000"];
-//   for (let i = 0; i <= arrStr.length; i++) {
-//     let num = String(arrStr[i]);
-//     let char = num[0];
-//     if (char == 1 || char == 2 || char == 5) {
-//       console.log(num);
-//     }
-//   }
-// }
+let CalcCasio = function () {
+  CalcSdc.apply(this, arguments);
 
-//Task 5
-// {
-//   let arrWeeks = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ];
-//   for (let i = 0; i < arrWeeks.length; i++) {
-//     let num = arrWeeks[i];
-//     console.log(num);
-//   }
-// }
+  this.dividing = function () {
+    let arrNum = [];
+    let numbers;
 
-//Task 6
-// {
-//   let arr = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ];
-//   arr.push("Another day");
-//   console.log(arr);
-//   console.log(arr[arr.length - 1]);
-// }
+    while (numbers !== 0 || "") {
+      numbers = +prompt("Введите значение");
+      arrNum.push(numbers);
+    }
+    arrNum.pop();
+    let dividingOfNum = arrNum.reduce(function (sum, elem) {
+      return sum / elem;
+    });
+  };
+};
 
-//Task 7
-// {
-//   let sum = [];
-//   let text = +prompt("Введите значения");
-//   while (text != "") {
-//     text = +prompt("Введите значения");
-//     sum.push(text);
-//   }
-//   sum.sort(function (a, b) {
-//     return a - b;
-//   });
-//   alert(sum);
-// }
-//Task 8
-// {
-//   let arr = [12, false, "Текст", 4, 2, -5, 0];
-//   arr.reverse();
-//   console.log(arr);
-// }
-//Task 9
-
-// {
-//   let arr = [5, 9, 21, , , 9, 78, , , , 6];
-//   let res = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (i in arr) {
-//       res.push(arr[i]);
-//     }
-//   }
-//   console.log(arr);
-//   console.log(res);
-// }
-//task 10
-
-{
-  let arr = [48, 9, 0, 4, 21, 2, 1, 0, 8, 84, 76, 8, 4, 13, 2];
-  const arr1 = arr.slice(3, 7);
-  let result = arr1.reduce((sum, elem) => sum + elem, 0);
-
-  console.log(arr1);
-  console.log(result);
-}
+let calcCasio = new CalcCasio("Calculator", "Casio");
