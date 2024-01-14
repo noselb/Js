fa0f6683c13154ecf54910cf5427272685db4851
noselb <amfibii@mail.ru>
2024-01-15 01:08:07 +0300

    add:hw13

--- scripts.js
+++ scripts.js
@@ -1,217 +1,269 @@
 "use strict";
 
-// //Task 1
-// {
-//   for (let i = 1; i <= 50; i++) {
-//     console.log(i);
-//   }
-// }
+class User {
+  constructor(userInfo) {
+    this.userInfo = userInfo;
+  }
 
-// {
-//   for (let j = 38; j >= 8; j--) {
-//     console.log(j);
-//   }
-// }
-// //Task 2
+  editUserInfo(newInfo) {
+    this.userInfo = newInfo;
+  }
 
-// {
-//   let i = 89;
-//   while (i >= 11) {
-//     document.write(i + "</br>");
-//     i--;
-//   }
-// }
-// //Task 3
-// {
-//   let sum = 0;
-//   for (let i = 0; i <= 100; i++) {
-//     console.log(sum + i);
-//     sum = sum + i;
-//   }
-// }
-// //Task 4
-// {
-//   let a = 5;
-//   let sum = 0;
-//   for (let i = 1; i <= a; i++) {
-//     sum = 0;
-//     for (let j = 1; j <= i; j++) {
-//       sum += j;
-//     }
-//     console.log("sum" + i + " = " + sum);
-//   }
-// }
-// //Task 5
-// {
-//   for (let i = 8; i <= 86; i++) {
-//     if (i % 2 == 0) continue;
-//     console.log(i);
-//   }
-// }
-
-// //Task 6
-// {
-//   for (let i = 2; i <= 10; i++) {
-//     for (let j = 2; j <= 10; j++) {
-//       console.log(`${i}*${j} = ${i * j}`);
-//     }
-//   }
-// }
-// //Task 7
-// {
-//   let n = 1000;
-//   let num = 0;
-//   while (n >= 50) {
-//     n = n / 2;
-//     num++;
-//     console.log(num);
-//   }
-// }
-// //Task 8
-// {
-//   let str = "";
-//   let num = 0;
-//   let sum = 0;
-//   let iteration = 0;
-//   while (true) {
-//     let user = prompt("Введите число с клавиатуры");
-//     sum = parseInt(user) + sum;
-//     iteration++;
-//     if (user == str || user == num) {
-//       break;
-//     }
-//   }
-//   console.log(`Сумма чисел равна ${sum}`);
-//   console.log(`Среднее арифметическое равно ${sum / iteration}`);
-// }
-// //Task 9
-
-//Array
-
-//Task 1
-// {
-//   let arr = [1, 2, 3, 4, 5];
-//   for (let i = 0; i < arr.length; i++) alert(arr[i]);
-// }
-// {
-//   let numbers = [1, 2, 3, 4, 5];
-//   for (let num of numbers) {
-//     alert(num);
-//   }
-// }
-
-//Task 2
-
-// {
-//   let arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
-//   alert(arr[13]);
-//   alert(arr[7]);
-//   alert(arr[5]);
-// }
-
-//Task 3
-
-// {
-//   let arr1 = [];
-//   let arr2 = [];
-//   let result = 0;
-//   let i = 23;
-//   let j = 0;
-
-//   for (; i <= 57; i++) {
-//     arr1.push(i);
-//     result += i;
-//   }
-//   console.log(arr1);
-//   console.log(result);
-
-// }
-
-//Task 4
-
-// {
-//   let arrStr = ["10", "20", "30", "50", "235", "3000"];
-//   for (let i = 0; i <= arrStr.length; i++) {
-//     let num = String(arrStr[i]);
-//     let char = num[0];
-//     if (char == 1 || char == 2 || char == 5) {
-//       console.log(num);
-//     }
-//   }
-// }
-
-//Task 5
-// {
-//   let arrWeeks = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ];
-//   for (let i = 0; i < arrWeeks.length; i++) {
-//     let num = arrWeeks[i];
-//     console.log(num);
-//   }
-// }
-
-//Task 6
-// {
-//   let arr = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ];
-//   arr.push("Another day");
-//   console.log(arr);
-//   console.log(arr[arr.length - 1]);
-// }
-
-//Task 7
-// {
-//   let sum = [];
-//   let text = +prompt("Введите значения");
-//   while (text != "") {
-//     text = +prompt("Введите значения");
-//     sum.push(text);
-//   }
-//   sum.sort(function (a, b) {
-//     return a - b;
-//   });
-//   alert(sum);
-// }
-//Task 8
-// {
-//   let arr = [12, false, "Текст", 4, 2, -5, 0];
-//   arr.reverse();
-//   console.log(arr);
-// }
-//Task 9
-
-// {
-//   let arr = [5, 9, 21, , , 9, 78, , , , 6];
-//   let res = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (i in arr) {
-//       res.push(arr[i]);
-//     }
-//   }
-//   console.log(arr);
-//   console.log(res);
-// }
-//task 10
-
-{
-  let arr = [48, 9, 0, 4, 21, 2, 1, 0, 8, 84, 76, 8, 4, 13, 2];
-  const arr1 = arr.slice(3, 7);
-  let result = arr1.reduce((sum, elem) => sum + elem, 0);
-
-  console.log(arr1);
-  console.log(result);
+  getUserInfo() {
+    return this.userInfo;
+  }
 }
+
+class Contacts {
+  constructor() {
+    this.users = [];
+    this.id = 0;
+  }
+
+  addContact(contact) {
+    const newContact = new User(contact);
+    this.id++;
+    newContact.id = this.id;
+    this.users.push(newContact);
+  }
+
+  editContact(id, obj) {
+    const user = this.users.find((user) => user.id === id);
+    user.editUserInfo(obj);
+  }
+
+  remove(id) {
+    const newArray = this.users.filter((user) => user.id != id);
+    this.users = newArray;
+  }
+
+  get() {
+    return this.users;
+  }
+}
+
+class ContactsApp extends Contacts {
+  constructor() {
+    super();
+    this.app = document.createElement("div");
+    this.app.classList.add("contacts");
+    document.body.appendChild(this.app);
+    this.createForm();
+
+    this.render();
+    this.getData();
+    // this.storageExpiration = null;
+  }
+
+  render() {
+    this.app.innerHTML = "";
+    this.users.forEach((user) => {
+      let editable = false;
+      let userInfo = user.getUserInfo();
+
+      let userContainer = document.createElement("div");
+      userContainer.classList.add("userContainer");
+
+      let userName = document.createElement("input");
+      userName.value = userInfo.name;
+      userName.classList.add("userName");
+      userContainer.append(userName);
+      userName.setAttribute("readonly", true);
+
+      let userAddress = document.createElement("input");
+      userAddress.value = userInfo.address;
+      userAddress.classList.add("userAddress");
+      userContainer.append(userAddress);
+      userAddress.setAttribute("readonly", true);
+
+      let userPhone = document.createElement("input");
+      userPhone.value = userInfo.phone;
+      userPhone.classList.add("userPhone");
+      userContainer.append(userPhone);
+      userPhone.setAttribute("readonly", true);
+
+      let userEmail = document.createElement("input");
+      userEmail.value = userInfo.email;
+      userEmail.classList.add("userEmail");
+      userContainer.append(userEmail);
+      userEmail.setAttribute("readonly", true);
+
+      let buttonContainer = document.createElement("div");
+      buttonContainer.classList.add("buttonContainer");
+
+      let buttonRemove = document.createElement("button");
+      buttonRemove.innerText = "Remove";
+      buttonRemove.addEventListener("click", this.onRemove);
+      buttonRemove.setAttribute("id", user.id);
+
+      let buttonEdit = document.createElement("button");
+      buttonEdit.innerText = "Edit";
+
+      buttonEdit.addEventListener("click", () => {
+        if (!editable) {
+          editable = !editable;
+          buttonEdit.innerText = "Ok";
+          userPhone.removeAttribute("readonly");
+          userEmail.removeAttribute("readonly");
+          userName.removeAttribute("readonly");
+          userAddress.removeAttribute("readonly");
+        } else {
+          editable = !editable;
+          buttonEdit.innerText = "Edit";
+          userPhone.setAttribute("readonly", true);
+          userEmail.setAttribute("readonly", true);
+          userName.setAttribute("readonly", true);
+          userAddress.setAttribute("readonly", true);
+
+          this.onEdit(user.id, {
+            name: userName.value,
+            phone: userPhone.value,
+            email: userEmail.value,
+            address: userAddress.value,
+            id: user.id,
+          });
+        }
+      });
+      buttonEdit.setAttribute("id", user.id);
+
+      buttonContainer.append(buttonEdit, buttonRemove);
+      userContainer.append(buttonContainer);
+
+      this.app.append(userContainer);
+    });
+  }
+
+  createForm() {
+    const formsContainer = document.createElement("div");
+    formsContainer.classList.add("forms-container");
+    document.body.append(formsContainer);
+
+    const forms = document.createElement("form");
+    forms.classList.add("forms");
+    formsContainer.append(forms);
+
+    const inputPhone = document.createElement("input");
+    inputPhone.setAttribute("type", "text");
+    inputPhone.classList.add("forms__input");
+    inputPhone.setAttribute("name", "phone");
+    inputPhone.setAttribute("placeholder", "3753303853");
+    forms.prepend(inputPhone);
+
+    const inputAddress = document.createElement("input");
+    inputAddress.setAttribute("type", "text");
+    inputAddress.classList.add("forms__input");
+    inputAddress.setAttribute("name", "address");
+    inputAddress.setAttribute("placeholder", "Minsk");
+    forms.prepend(inputAddress);
+
+    const inputName = document.createElement("input");
+    inputName.setAttribute("type", "text");
+    inputName.classList.add("forms__input");
+    inputName.setAttribute("name", "name");
+    inputName.setAttribute("placeholder", "Andrew");
+    forms.prepend(inputName);
+
+    let inputEmail = document.createElement("input");
+    inputEmail.setAttribute("type", "text");
+    inputEmail.classList.add("forms__input");
+    inputEmail.setAttribute("name", "email");
+    inputEmail.setAttribute("placeholder", "Email");
+    forms.append(inputEmail);
+
+    const title = document.createElement("h1");
+    title.classList.add("forms__title");
+    title.innerHTML = "Write your data";
+    forms.prepend(title);
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("forms__buttonContainer");
+    forms.append(buttonContainer);
+
+    const button = document.createElement("button");
+    button.classList.add("forms__button");
+    button.innerHTML = "Add";
+    buttonContainer.append(button);
+
+    button.addEventListener("click", this.onAdd);
+  }
+
+  onAdd = (event) => {
+    event.preventDefault();
+    let searchEmail = document.querySelector('[name = "email"]');
+    let emailValue = searchEmail.value;
+    let searchPhone = document.querySelector('[name = "phone"]');
+    let phoneValue = searchPhone.value;
+    let searchAddress = document.querySelector('[name = "address"]');
+    let addressValue = searchAddress.value;
+    let searchName = document.querySelector('[name = "name"]');
+    let nameValue = searchName.value;
+    const dataFields = {
+      email: emailValue,
+      phone: phoneValue,
+      address: addressValue,
+      name: nameValue,
+    };
+    let forms = document.querySelectorAll(".forms__input");
+    let res = Array.from(forms).forEach(function (elem) {
+      elem.value = "";
+    });
+
+    this.addContact(dataFields);
+    this.render();
+
+    console.log(this.users);
+    this.updateStorage();
+  };
+
+  onEdit = (id, newInfo) => {
+    this.editContact(id, newInfo);
+    this.render();
+    this.updateStorage();
+  };
+
+  onRemove = (event) => {
+    let idUser = event.currentTarget.getAttribute("id");
+    this.remove(idUser);
+    this.render();
+    this.updateStorage();
+  };
+
+  updateStorage() {
+    let storageData = JSON.stringify(this.users);
+    if (typeof storageData == "string")
+      localStorage.setItem("data", storageData);
+    this.setStorageExpirationCookie(10);
+  }
+
+  setStorageExpirationCookie(days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `storageExpiration=${date.toUTCString()};path=/;expires=${date.toUTCString()}`;
+  }
+
+  clearLocalStorageAndCookies() {
+    localStorage.removeItem("data");
+    document.cookie =
+      "storageExpiration=; expires=Thu, 15 Jan 1870 00:00:00 UTC; path=/;";
+  }
+
+  async getData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    const item = await data.forEach((item) => {
+      this.render();
+      const serverContacts = {
+        id: this.id,
+        name: item.name,
+        email: item.email,
+        address: item.address.street,
+        phone: item.phone,
+      };
+      this.addContact(serverContacts);
+    });
+    this.updateStorage();
+  }
+}
+
+let contactApp = new ContactsApp();
+//contactApp.clearLocalStorageAndCookies()
